049952763d3d04681720165e80a815ae2c31adee
Reddy, Prasad <Prasad.Reddy@Teradata.com>
2018-08-30 17:23:46 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CardsModule } from './cards/cards.module';
+import { CardsModule } from '../lib/public_api';
 
 @NgModule({
   declarations: [
