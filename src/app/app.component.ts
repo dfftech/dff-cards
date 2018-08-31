fa16ee5bb5331766b4f936d3fefd1ad553f83c98
prasadsrg <prasad.srg@gmail.com>
2018-08-31 22:19:06 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'dff-root',
+  selector: "dff-root",
   template: `
-    <dff-card-aa></dff-card-aa>
+    <dff-card-aa [inputData]="cardAaData"></dff-card-aa>
   `,
-  styles: []
+  styles: [
+    `
+      h2 {
+        font-size: 1em !important;
+        -webkit-margin-before: 0em !important;
+        -webkit-margin-after: 0em !important;
+        -webkit-margin-start: 0px !important;
+        -webkit-margin-end: 0px !important;
+      }
+    `
+  ]
 })
 export class AppComponent {
-  title = 'dff-cards';
+  title = "dff-cards";
+  cardAaData = {
+    img: "https://d38y6a0t4hqpxx.cloudfront.net/images/july8-cover.jpg",
+    name: "card-aa",
+    height: "300px",
+    bg: "blue",
+    summary:
+      " abcd def ghtasdf ads asdf as dfa sd fa sd fa sdfasdfasdfas asdfasdfasd.",
+    data: {
+      id: 1
+    }
+  };
 }
--- src/lib/card-a/components/card-aa.component.ts
+++ src/lib/card-a/components/card-aa.component.ts
@@ -1,19 +1,74 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'dff-card-aa',
+  selector: "dff-card-aa",
   template: `
-    <div class='card-aa'>
-      card-aa works!
+    <div class='card-aa' [style.height]="inputData.height" (click)="onCardSelect()">
+      <div class="img-box">
+        <img [src]="inputData.img">
+      </div>
+      <div class="box">
+          <h3 style="text-align: center">{{inputData.name}}</h3>
+          <p>{{inputData.summary}}
+      </div>
     </div>
   `,
-  styles: [`.card-aa{}`]
+  styles: [
+    `
+      .card-aa {
+        background: transparent;
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+        cursor: pointer;
+      }
+      .img-box {
+        width: 100%;
+        height: auto;
+        margin: 0 auto;
+        overflow: hidden;
+        background: transparent;
+      }
+      .img-box img {
+        position: relative;
+        height: auto;
+        left: 50%;
+        -moz-transform: translate(-50%);
+        -ms-transform: translate(-50%);
+        -webkit-transform: translate(-50%);
+        transform: translate(-50%);
+      }
+
+      .card-aa .box {
+        position: relative;
+        bottom: 0px;
+        width: 100%;
+        height: 100px;
+      }
+      .card-aa .box p {
+        font-size: 12px;
+        text-align: center;
+      }
+      .card-aa .box h3 {
+        -webkit-margin-before: 0em;
+        -webkit-margin-after: 0em;
+        -webkit-margin-start: 0px;
+        -webkit-margin-end: 0px;
+      }
+    `
+  ]
 })
 export class CardAaComponent implements OnInit {
-  @Input() inputData; @Output() outputEmitter: EventEmitter<any> = new EventEmitter();
-  constructor() { }
+  @Input()
+  inputData: any = {};
+  @Output()
+  outputEmitter: EventEmitter<any> = new EventEmitter();
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  onCardSelect() {
+    if (this.inputData && this.inputData.data) {
+      this.outputEmitter.emit(this.inputData.data);
+    }
   }
-  onCardSelect() { if(this.inputData && this.inputData.data){ this.outputEmitter.emit(this.inputData.data)}}
 }
