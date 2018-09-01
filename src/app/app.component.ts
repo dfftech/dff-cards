7ae10d0686b8ad2383569e98909c009139378f07
tharun0507 <tharunesh@techmojo.in>
2018-09-01 19:51:20 +0530

    cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { Component } from "@angular/core";
 @Component({
   selector: "dff-root",
   template: `
-    <dff-card-aa [inputData]="cardAaData"></dff-card-aa>
-    <dff-card-ab [inputData]="cardAbData"></dff-card-ab>
+  
+    <dff-card-bc [inputData]="cardBcData"></dff-card-bc>
   `,
-  styles: [``]
+  styles: [`
+  
+  `]
 })
 export class AppComponent {
   title = "dff-cards";
@@ -28,5 +30,26 @@ export class AppComponent {
     img:
       "https://digital-lync.com/assets/images/courses/devopscourse/linux-digital-lync.svg",
     imgAlt: "DevOps"
+  };
+
+
+  cardBaData = {
+    title: "banner",
+    subTitle:"Classroom & Online",
+    video:"https://s3.ap-south-1.amazonaws.com/dl-web-assets/Digital_Lync_Landing_Page_Video.mp4",
+    videoPoster:"https://s3.ap-south-1.amazonaws.com/dl-web-assets/Digital_Lync_Landing_Page_Video.mp4"
+  };
+
+  cardBbData = {
+    title: "banner",
+    img:"https://d38y6a0t4hqpxx.cloudfront.net/images/devops-cover-digital-lync.png",
+    video:"https://www.youtube.com/watch?v=JQmAn9eNtNI&t=12s"
+  };
+
+  cardBcData = {
+    title: "Events at Digital Lync.",
+    img:"https://d38y6a0t4hqpxx.cloudfront.net/images/data-science-digital-lync.png",
+    description:"Talk to industry leaders and know real facts. Clear your queries and walk out with a career decision.Do attend our workshops and seminars. Let us meet up!",
+    video:"https://www.youtube.com/watch?v=JQmAn9eNtNI&t=12s"
   };
 }
