64cce042baea0fa502b71cae86d3ef6f5584ec86
Reddy, Prasad <Prasad.Reddy@Teradata.com>
2018-09-25 17:22:31 +0530

    commit

--- src/lib/cards/components/card-e/card-e.component.ts
+++ src/lib/cards/components/card-e/card-e.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'dff-card-e',
-  templateUrl: './card-e.component.html',
-  styleUrls: ['./card-e.component.css']
+  selector: "dff-card-e",
+  templateUrl: "./card-e.component.html",
+  styleUrls: ["./card-e.component.css"]
 })
 export class CardEComponent implements OnInit {
+  @Input()
+  inputData: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  @Output()
+  outputEmitter: EventEmitter<any> = new EventEmitter();
+  constructor() {
+    if (!this.inputData) {
+      this.inputData = {};
+    }
   }
 
+  ngOnInit() {}
+
+  clickOnSubmit() {
+    this.outputEmitter.emit(this.inputData);
+  }
 }
