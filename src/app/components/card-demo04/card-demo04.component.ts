ac7e1b4372691ab9b9c0b27ce27bae87db98a296
Reddy, Prasad <Prasad.Reddy@Teradata.com>
2018-09-26 16:42:46 +0530

    commit

--- src/app/components/card-demo04/card-demo04.component.ts
+++ src/app/components/card-demo04/card-demo04.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'dff-card-demo04',
-  templateUrl: './card-demo04.component.html',
-  styleUrls: ['./card-demo04.component.css']
+  selector: "dff-card-demo04",
+  templateUrl: "./card-demo04.component.html",
+  styleUrls: ["./card-demo04.component.css"]
 })
 export class CardDemo04Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  inputData = {};
+  outputEmitter($event: any) {
+    console.log($event);
   }
-
 }
--- src/app/components/card-demo06/card-demo06.component.ts
+++ src/app/components/card-demo06/card-demo06.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'dff-card-demo06',
-  templateUrl: './card-demo06.component.html',
-  styleUrls: ['./card-demo06.component.css']
+  selector: "dff-card-demo06",
+  templateUrl: "./card-demo06.component.html",
+  styleUrls: ["./card-demo06.component.css"]
 })
 export class CardDemo06Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  inputData = {};
+  outputEmitter($event: any) {
+    console.log($event);
   }
 
+  htmlContent1 =
+    ' <dff-card-e [(inputData)]="inputData" (outputEmitter)="outputEmitter($event)"></dff-card-e>';
 }
