809f51eacfaf6d747521a17504aad7bed54a4c19
Reddy, Prasad <Prasad.Reddy@Teradata.com>
2019-01-18 17:11:58 +0530

    commit

--- src/app/components/card-demo04/card-demo04.component.ts
+++ src/app/components/card-demo04/card-demo04.component.ts
@@ -1,17 +1,65 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "dff-card-demo04",
-  templateUrl: "./card-demo04.component.html",
-  styleUrls: ["./card-demo04.component.css"]
+    selector: "dff-card-demo04",
+    templateUrl: "./card-demo04.component.html",
+    styleUrls: ["./card-demo04.component.css"]
 })
 export class CardDemo04Component implements OnInit {
-  constructor() {}
+    constructor() {}
 
-  ngOnInit() {}
+    ngOnInit() {}
 
-  inputData = {};
-  outputEmitter($event: any) {
-    console.log($event);
-  }
+    inputData = {};
+    outputEmitter($event: any) {
+        console.log($event);
+    }
+
+    htmlContent1 = `
+  <dff-card-s>
+            <main style="background-color: red">
+                <h2>Main content</h2>
+                <p>Scroll down the page!</p>
+                <h3>How to recreate this</h3>
+
+                <h2>Main content</h2>
+                <p>Scroll down the page!</p>
+                <h3>How to recreate this</h3>
+
+                <h2>Main content</h2>
+                <p>Scroll down the page!</p>
+                <h3>How to recreate this</h3>
+
+                <h2>Main content</h2>
+                <p>Scroll down the page!</p>
+                <h3>How to recreate this</h3>
+
+                <h2>Main content</h2>
+                <p>Scroll down the page!</p>
+                <h3>How to recreate this</h3>
+
+                <h2>Main content</h2>
+                <p>Scroll down the page!</p>
+                <h3>How to recreate this</h3>
+
+                <h2>Main content</h2>
+                <p>Scroll down the page!</p>
+                <h3>How to recreate this</h3>
+
+                <h2>Main content</h2>
+                <p>Scroll down the page!</p>
+                <h3>How to recreate this</h3>
+
+                <h2>Main content</h2>
+                <p>Scroll down the page!</p>
+                <h3>How to recreate this</h3>
+            </main>
+            <aside
+                style="background-color: green"
+                style="height: 200px; overflow: hidden;"
+            >
+                <h3>Sticky sidebar</h3>
+            </aside>
+        </dff-card-s>
+        `;
 }
